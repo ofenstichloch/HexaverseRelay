8d05b969d29951e98b6ab49b77f02d4a65fda2ba
Alexander Landmesser <ofenstichloch@land-server.de>
2015-12-10 15:54:15 +0100

    init

---
+++ SpectateServer/Relay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace SpectateServer
+{
+	public class Relay
+	{
+		private static String HOST;
+		private static int PORT;
+
+		private Thread thread;
+		private 
+
+		public void Relay ()
+		{
+			thread = new Thread (listen ());
+			loginToSever ();
+			thread.Start ();
+		}
+
+		private void listen(){
+
+		}
+
+		private bool loginToSever(){
+			
+			return false;
+		}
+	}
+}
+
