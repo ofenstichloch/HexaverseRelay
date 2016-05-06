a344c4b25ba2cf9524af84203de3ce9433d5ff72
ofenstichloch <stuff@land-server.de>
2016-05-06 12:16:57 +0200

    Buffer for 10 rounds

--- SpectateServer/MessageBuffer.cs
+++ SpectateServer/MessageBuffer.cs
@@ -6,46 +6,45 @@ namespace SpectateServer
 {
     class MessageBuffer
     {
+        const int ROUNDSTOSAVE = 10;
+        const int DELAY = 5;
         Host host;
-        private List<byte[]> buffer;
+        //private List<byte[]> buffer;
         uint firstRound;
+        uint lastRound;
+        bool empty;
         Semaphore access;
         Semaphore notReady;
         bool initialized = false;
+
+        Queue<int> roundPointer;
+        Queue<byte[]> buffer;
+        bool hasDelayedData;
         public MessageBuffer(Host host)
         {
             this.host = host;
-            buffer = new List<byte[]>();
-            firstRound = 0;
-            access = new Semaphore(1, 1);
-            notReady = new Semaphore(0, 1);
+            buffer = new Queue<byte[]>();
+            roundPointer = new Queue<int>(ROUNDSTOSAVE);
+            access = new Semaphore(1, 1);   
+            notReady = new Semaphore(0, 1); //Start blocked, queue has nothing to serve
         }
 
         public void add(byte[] paket)
         {
             int channel = BitConverter.ToInt32(paket, 0);
-            if (channel == (int)Protocol.ChannelID.BeginEverything)
+            if (channel == (int)Protocol.ChannelID.PhaseChange)
             {
-                Log.notify("Start filling cache",this);
-                if (initialized) notReady.WaitOne();
-                initialized = false;
-                buffer.Clear();
-                access.WaitOne();
+             
             }
-            else if (channel == (int)Protocol.ChannelID.EndEverything)
-            {
-                Log.notify("Finished filling cache with "+buffer.Count+" messages", this);
-                initialized = true;
-                access.Release();
-                notReady.Release();
-            }
-
-            if(buffer.Count >= 200)
+            if(lastRound > firstRound+ROUNDSTOSAVE)
             {
                 Log.notify("Cache went full, resetting", this);
                 notReady.WaitOne();
                 initialized = false;
                 buffer.Clear();
+                firstRound = 0;
+                lastRound = 0;
+                empty = true;
                 return;
             }
 
